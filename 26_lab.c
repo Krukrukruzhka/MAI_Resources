4d4a8b0c8e036ae2f3f3cc988ea2e39cd0232a94
Mikhail <46652348+Krukrukruzhka@users.noreply.github.com>
2022-05-27 16:44:09 +0300

    fixed 27.05.2022

--- 26_lab.c
+++ 26_lab.c
@@ -30,22 +30,15 @@ dequeue *make_dequeue(int input) {
 	return deq;
 }
 
-dequeue *make_empty_dequeue() {
-    dequeue *deq = (dequeue *) malloc(sizeof(dequeue));
-    deq->left = NULL;
-    deq->right = NULL;
-    deq->len = 0;
-	return deq;
-}
-
 void push_back(dequeue *deq, int input) {
 	element *new_element = (element *) malloc(sizeof(element));
 	new_element->data = input;
 	new_element->right = NULL;
 	new_element->left = deq->tail;
-	deq->tail->right = new_element;
+	if (deq->len != 0)
+	    deq->tail->right = new_element;
 	deq->tail = new_element;
-		if (deq->len == 0)
+	if (deq->len == 0)
 		deq->head = new_element;
 	deq->len++;
 }
@@ -55,7 +48,8 @@ void push_front(dequeue *deq, int input) {
 	new_element->data = input;
 	new_element->left = NULL;
 	new_element->right = deq->head;
-	deq->head->left = new_element;
+	if (deq->len != 0)
+	    deq->head->left = new_element;
 	deq->head = new_element;
 	if (deq->len == 0)
 		deq->tail = new_element;
@@ -66,7 +60,7 @@ void print_dequeue(dequeue *deq) {
 	element *iterator = deq->head;
 	while (iterator != NULL) {
 		printf("%d ", iterator->data);
-		iterator = iterator->left;
+		iterator = iterator->right;
 	}
 }
 
@@ -75,7 +69,8 @@ int pop_front(dequeue *deq, bool clear) {
 	if (clear) {
 		element *delete_element = deq->head;
 		deq->head = deq->head->right;
-		deq->head->left = NULL;
+	    if (deq->head != NULL)
+		    deq->head->left = NULL;
 		if (deq->len == 1) {
 			deq->tail = NULL;
 		}
@@ -85,54 +80,48 @@ int pop_front(dequeue *deq, bool clear) {
 	return output;
 }
 
-dequeue *sort_deq(dequeue *deq) {
-    int step = 2;
-    int n = deq->len;
-    dequeue **t = malloc(sizeof(dequeue)*n);
-    for(int i = 0; i<n; i++) {
-        dequeue *f = make_empty_dequeue();
-        push_front(f, pop_front(d,) -> value); // EEEEEEEEEEEEEEEE
-        t[i] = f;
-    }
-    int b = n;
-    printf("Sorting =======\n");
-    while(b > 1) {
-        int i = 0;
-        int k = 0;
-        while(i < b) {
-            dequeue *f = make_empty_dequeue();
-            int count = (int)deck_size(t[i]) + (int)deck_size(t[i+1]); // EEEEEEEEEEEEEEEEEEEEEEEEE
-            for(int j = 0; j<count; j++) {
-                if(deck_is_empty(t[i])) { // EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-                    unit *u = deck_pop_front(t[i+1]); // EEEEEEEEEEEEEEEEEEEEE
-                    push_back(f, u->data);
-                    free(u);
-                }
-                else if (deck_is_empty(t[i+1])) { // EEEEEEEEEEEEEEEEEEE
-                    unit *u = deck_pop_front(t[i]); // EEEEEEEEEEEEEEEEEEEEE
-                    push_back(f, u->data);
-                    free(u);
-                }
-                else if(t[i]->left->value > t[i+1]->left->value) { // EEEEEEEEEEEEEEEEEEE
-                    unit *u = deck_pop_front(t[i+1]); // EEEEEEEEEEEEEEEEEEEEEEEE
-                    push_back(f, u->data);
-                    free(u);
-                }
-                else {
-                    unit *u = deck_pop_front(t[i]); // EEEEEEEEEEEEEEEEEEEEE
-                    push_back(f, u->data);
-                    free(u);
-                }
-            }
-            print_dequeue(f);
-            t[k] = f;
-            k++;
-            i+=step;
+dequeue *merge(dequeue *deq1, dequeue *deq2) {
+    dequeue *deq = malloc(sizeof(dequeue));
+    while (true)
+        if(deq1->len != 0 && deq2->len != 0)
+            if (pop_front(deq1, 0) <= pop_front(deq2, 0))
+                push_back(deq, pop_front(deq1, 1));
+            else
+                push_back(deq, pop_front(deq2, 1));
+        else if (deq1->len != 0)
+            push_back(deq, pop_front(deq1, 1));
+        else if (deq2->len != 0)
+            push_back(deq, pop_front(deq2, 1));
+        else {
+            // free(deq1);
+            // free(deq2);
+            return deq;
         }
-        b = b / step + (b % step);
-        printf("============\n");
+}
+
+dequeue *sorted_dequeue(dequeue *deq) {
+    dequeue *merged_deq;
+    if (deq->len == 1) {
+        merged_deq = make_dequeue(pop_front(deq, 1));
+        free(deq);
+        return merged_deq;    
     }
-    return t[0];
+    else {
+        dequeue *deq1 = make_dequeue(pop_front(deq, 1));
+        dequeue *deq2 = make_dequeue(pop_front(deq, 1));
+        int k = deq->len;
+        for(int i = 0; i < k/2; i++) {
+            push_back(deq1, pop_front(deq, 1));
+            push_back(deq2, pop_front(deq, 1));
+        }
+        if (deq->len != 0)
+            push_back(deq2, pop_front(deq, 1));
+        free(deq);
+        deq1 = sorted_dequeue(deq1);
+        deq2 = sorted_dequeue(deq2);
+        merged_deq = merge(deq1, deq2);
+        return merged_deq;    
+    }
 }
 
 int pop_back(dequeue *deq, bool clear) {
@@ -140,7 +129,8 @@ int pop_back(dequeue *deq, bool clear) {
 	if (clear) {
 		element *delete_element = deq->tail;
 		deq->tail = deq->tail->left;
-		deq->tail->right = NULL;
+		if (deq->tail != NULL)
+		    deq->tail->right = NULL;
 		if (deq->len == 1) {
 			deq->head = NULL;
 		}
@@ -153,74 +143,89 @@ int pop_back(dequeue *deq, bool clear) {
 
 int main(void) {
 	dequeue *deq;
+	printf("Enter the command:\n1 Create dequeue\n2 Push in tail\n3 Get value from tail\n4 Push in head\n5 Get value from head\n6 Sort dequeue\n7 Print dequeue\n\n");
 	while (true) {
 		int command;
-		printf("Enter the command:\n1 Create dequeue\n2 Push in tail\n3 Get value from tail\n4 Push in head\n5 Get value from head\n6 Sort dequeue\n7 Print dequeue\n\n");
 		scanf("%d", &command);
-			switch(command):
-				case 1:
-				{
-					printf("...Enter value...\n");
-					int input;
-					scanf("%d", &input);
-					deq = make_dequeue(input);
-					printf("...Dequeue was created...\n\n");
-					break;
+		switch(command) {
+			case 1:
+			{
+				printf("...Enter value...\n");
+				int input;
+				scanf("%d", &input);
+				deq = make_dequeue(input);
+				printf("...Dequeue was created...\n\n");
+				break;
+			}
+			case 2:
+			{
+			    printf("...Enter value...\n");
+				int input;
+				scanf("%d", &input);
+				push_back(deq, input);
+				printf("...Element was added...\n\n");   
+				break;
+			}
+			case 3:
+			{
+			    if (deq->head == NULL) {
+			        printf("!!!Dequeue is empty!!!\n\n");
+			    } else {
+    			    printf("...Delete the last element? (1-yes, 0-no)...\n");
+    				int input;
+    				scanf("%d", &input);
+    				printf("%d \n\n", pop_back(deq, input));
+			    }
+			    break;
+			}
+			case 4:
+			{
+			    printf("...Enter value...\n");
+				int input;
+				scanf("%d", &input);
+				push_front(deq, input);
+				printf("...Element was added...\n\n");
+				break;
+			}
+			case 5:
+			{
+			    if (deq->head == NULL)
+			        printf("!!!Dequeue is empty!!!\n\n");
+				else {
+    				printf("...Delete the first element? (1-yes, 0-no)...\n");
+    				int input;
+    				scanf("%d", &input);
+    				printf("%d \n\n", pop_front(deq, input));
 				}
-				case 2:
-				{
-					printf("...Enter value...\n");
-					int input;
-					scanf("%d", &input);
-					push_back(deq, input);
-					printf("...Element was added...\n\n");
-					break;
+				break;	
+			}
+			case 6:
+			{
+				if (deq->len == 0)
+				    printf("!!!Dequeue is empty!!!\n\n");
+				else
+				    deq = sorted_dequeue(deq);
+				    printf("...Dequeue was sorted...\n\n");
+				break;
+			}
+			case 7:
+			{
+				if (deq->head == NULL) {
+					printf("!!!Dequeue is empty!!!\n\n");
+				} else {
+					print_dequeue(deq);
+					printf("\nLength = %d", deq->len);
+					printf("\n\n");	
 				}
-				case 3:
-				{
-					printf("...Delete the last element? (1-yes, 0-no)...\n");
-					int input;
-					scanf("%d", &input);
-					printf("%d \n\n", pop_back(deq, input));
-					break;
-				}
-				case 4:
-				{
-					printf("...Enter value...\n");
-					int input;
-					scanf("%d", &input);
-					push_front(deq, input);
-					printf("...Element was added...\n\n");
-					break;
-				}
-				case 5:
-				{
-					printf("...Delete the first element? (1-yes, 0-no)...\n");
-					int input;
-					scanf("%d", &input);
-					printf("%d \n\n", pop_front(deq, input));
-					break;	
-				}
-				case 6:
-				{
-					
-					break;
-				}
-				case 7:
-				{
-					if (deq->head == NULL) {
-						printf("!!!Dequeue is empty!!!\n\n");
-					} else {
-						print_dequeue(deq);
-						printf("\n\n");	
-					}
-					break;
-				}
-				default:
-				{
-					printf("!!!Incorrect command!!!\n\n");
-					break;
-				}
-		
+				break;
+			}
+			default:
+			{
+				printf("!!!Incorrect command!!!\n\n");
+				break;
+			}
+		}
 	}
+	return 0;
 }
+
